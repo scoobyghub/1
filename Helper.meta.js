e2a2943ca475539070d174aa33f93c32cd344c2d
Scooby <tmnsparky@hotmail.co.uk>
2026-02-12 21:06:46 +0000

    Update version to 12.16 and description

--- Helper.meta.js
+++ Helper.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         TMN 2010 Automation Helper v12.15
+// @name         TMN 2010 Automation Helper v12.16
 // @namespace    http://tampermonkey.net/
-// @version      12.15
-// @description  v12.15 + Logout alerts trigger on login page
+// @version      12.16
+// @description  v12.16 + Logout alerts trigger on login page
 // @author       You
 // @match        *://www.tmn2010.net/login.aspx*
 // @match        *://www.tmn2010.net/authenticated/*
