27c66a1cd4c7ca56f0244887868955d4e7c04028
Scooby <tmnsparky@hotmail.co.uk>
2026-02-13 08:28:50 +0000

    Bump version to 12.18 and update description

--- Helper.meta.js
+++ Helper.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         TMN 2010 Automation Helper v12.17
+// @name         TMN 2010 Automation Helper v12.18
 // @namespace    http://tampermonkey.net/
-// @version      12.17
-// @description  v12.17 + Logout alerts trigger on login page
+// @version      12.18
+// @description  v12.18 + Logout alerts trigger on login page
 // @author       You
 // @match        *://www.tmn2010.net/login.aspx*
 // @match        *://www.tmn2010.net/authenticated/*
